72eeb32dedbb331a9f5048fb22013dc1021e4919
Vinesma <otaviocos_13@hotmail.com>
2019-12-18 18:12:17 -0300

    code cleanup

--- File rename/renameScript.py
+++ File rename/renameScript.py
@@ -1,18 +1,18 @@
 import shutil, os, re, sys, json
 
-namePattern = re.compile(r'(\[.+\]\s)([a-zA-Z0-9\'"@%+&()!. -]+)(\s.+)(\.mkv|\.mp4)') # Compile regex
-namePattern2 = re.compile(r'(\.mkv|\.mp4)') # Compile regex
+mainRegex = re.compile(r'(\[.+\]\s)([a-zA-Z0-9\'"@%+&()!. -]+)(\s.+)(\.mkv|\.mp4)')
+videoFileRegex = re.compile(r'(\.mkv|\.mp4)')
 absWorkingDir = os.path.abspath('.') # Get the absolute filepath for the working dir
-opt = 6 # Options
+chosenOption = 6
 
 # Reading config files
 try:
     if sys.platform == 'linux':
         with open("configLinux.json", "r") as readFile:
-            dstFolder, dstFolder2 = json.load(readFile)
+            destinationFolder, destinationFolder2 = json.load(readFile)
     else:
         with open("configWin.json", "r") as readFile:
-            dstFolder, dstFolder2 = json.load(readFile)
+            destinationFolder, destinationFolder2 = json.load(readFile)
 except FileNotFoundError as err:
     print("[config] configuration not found, creating templates now.")
     template = ["<PATH to SEASONALS folder>", "<PATH to REGULAR EPS folder>"]
@@ -20,112 +20,115 @@ except FileNotFoundError as err:
         json.dump(template, openFile)
     with open("configWin.json", "w") as openFile:
         json.dump(template, openFile)
-    opt = 5
+    chosenOption = 5
 
-def loopDir():
-    # Loop between all the files in the current dir
-    # Returns a List
-    returnList = []
-    for filename in os.listdir('.'):
-        returnList.append(filename)
-    return returnList
-
-def renameCopyMoveFunc():
-    # Renames, copies, then moves the file into a "Watched" folder
+# Renames, copies, then moves the file into a "./Watched/"
+def renameCopyMove():
+    # Checks if "./Watched/" exists, if not it is created
     if not(os.path.isdir(os.path.join('.', 'Watched'))):
         os.mkdir(os.path.join('.', 'Watched'))
     
-    fileList = loopDir()
-    for filename in fileList:
-        mo = namePattern.search(filename) # Search for the regex
+    fileList = loopFilesInDir()
+    for fileName in fileList:
+        mo = mainRegex.search(fileName)
 
         if mo == None: # Skip files that don't match the regex
             continue
 
-        title = mo.group(2)
-        extension = mo.group(4) # Get the different parts of the filename
+        fileTitle = mo.group(2)
+        fileExtension = mo.group(4)
+        newFileName = fileTitle + fileExtension
 
-        newFilename = title + extension # Form the new filename
-
-        filename = os.path.join(absWorkingDir, filename)
-        newFilename = os.path.join(absWorkingDir, newFilename) # From
-        print('Renaming: {}\nInto: {}'.format(filename, newFilename))
-        shutil.move(filename, newFilename) # Rename
-        print('Copying into: {}'.format(dstFolder))
-        shutil.copy(newFilename, dstFolder) # To
+        fileName = os.path.join(absWorkingDir, fileName)
+        newFileName = os.path.join(absWorkingDir, newFileName)
+        print('Renaming: {}\nInto: {}'.format(fileName, newFileName))
+        shutil.move(fileName, newFileName) # Rename
+        print('Copying into: {}'.format(destinationFolder))
+        shutil.copy(newFileName, destinationFolder) # Copy
         print('And moving into Watched folder')
-        shutil.move(newFilename, os.path.join('.', 'Watched')) # Move
+        shutil.move(newFileName, os.path.join('.', 'Watched')) # Move
         print('+---------------------------------------+')
+
     print('RENAME/COPY/MOVE SCRIPT FINISHED!')
     if sys.platform != 'linux':
         input('Press ENTER to exit...')
 
-def renameCopyFunc():
-    # Renames the files
-    fileList = loopDir()
-    for filename in fileList:
-        mo = namePattern.search(filename) # Search for the regex
+# Renames the files, then copies them to a folder
+def renameCopy():
+    fileList = loopFilesInDir()
+    for fileName in fileList:
+        mo = mainRegex.search(fileName)
 
         if mo == None: # Skip files that don't match the regex
             continue
 
-        title = mo.group(2)
-        extension = mo.group(4) # Get the different parts of the filename
+        fileTitle = mo.group(2)
+        fileExtension = mo.group(4)
+        newFileName = fileTitle + fileExtension
 
-        newFilename = title + extension # Form the new filename
+        fileName = os.path.join(absWorkingDir, fileName)
+        newFileName = os.path.join(absWorkingDir, newFileName) # From
 
-        filename = os.path.join(absWorkingDir, filename)
-        newFilename = os.path.join(absWorkingDir, newFilename) # From
-        print('Renaming: {}\nInto: {}'.format(filename, newFilename))
-        shutil.move(filename, newFilename) # Rename
+        print('Renaming: {}\nInto: {}'.format(fileName, newFileName))
+        shutil.move(fileName, newFileName) # Rename
+        print('Copying into: {}'.format(destinationFolder2))
+        shutil.copy(newFileName, destinationFolder2) # Copy
         print('+---------------------------------------+')
+
     print('RENAME/COPY SCRIPT FINISHED!')
     if sys.platform != 'linux':
         input('Press ENTER to exit...')
 
-def copyFunc():
-    # Copies the files to dstFolder2
-    fileList = loopDir()
-    for filename in fileList:
-        mo = namePattern2.search(filename) # Search for .mkv/.mp4 files
+def copy():
+    # Copies the files to destinationFolder2
+    fileList = loopFilesInDir()
+    for fileName in fileList:
+        mo = videoFileRegex.search(fileName) # Search for .mkv/.mp4 files
 
         if mo == None: # Skip files that don't match the regex
             continue
 
-        filename = os.path.join(absWorkingDir, filename) # From
-        print('Copying: {}\nTo: {}'.format(filename, dstFolder2))
-        shutil.copy(filename, dstFolder2) # To
+        fileName = os.path.join(absWorkingDir, fileName) # From
+        print('Copying: {}\nTo: {}'.format(fileName, destinationFolder2))
+        shutil.copy(fileName, destinationFolder2) # Copy
         print('+---------------------------------------+')
+
     print('COPY SCRIPT FINISHED!')
     if sys.platform != 'linux':
         input('Press ENTER to exit...')
 
-def renameFunc():
+def rename():
     # Renames files into their own dir   
-    fileList = loopDir()
-    for filename in fileList:
-        mo = namePattern.search(filename) # Search for the regex
+    fileList = loopFilesInDir()
+    for fileName in fileList:
+        mo = mainRegex.search(fileName)
 
         if mo == None: # Skip files that don't match the regex
             continue
 
-        title = mo.group(2)
-        extension = mo.group(4) # Get the different parts of the filename
+        fileTitle = mo.group(2)
+        fileExtension = mo.group(4)
+        newFileName = fileTitle + fileExtension
 
-        newFilename = title + extension # Form the new filename
-
-        filename = os.path.join(absWorkingDir, filename)
-        newFilename = os.path.join(absWorkingDir, newFilename) # From
-        print('Renaming: {}\nInto: {}'.format(filename, newFilename))
-        shutil.move(filename, newFilename) # Rename
+        fileName = os.path.join(absWorkingDir, fileName)
+        newFileName = os.path.join(absWorkingDir, newFileName) # From
+        print('Renaming: {}\nInto: {}'.format(fileName, newFileName))
+        shutil.move(fileName, newFileName) # Rename
         print('+---------------------------------------+')
+
     print('RENAME SCRIPT FINISHED!')
     if sys.platform != 'linux':
         input('Press ENTER to exit...')
 
+def loopFilesInDir():
+    returnList = []
+    for fileName in os.listdir('.'):
+        returnList.append(fileName)
+    return returnList
+
 # START
 
-while opt == 6:
+while chosenOption == 6:
     print("Welcome! This is a script for moving my weeb shit to a flash drive:")
     print("[config] Your OS has been identified as: {}".format(sys.platform))
     print("1. Rename files, copy to USB and then store them in a folder called 'Watched'")
@@ -134,23 +137,23 @@ while opt == 6:
     print("4. Rename only")
     print("5. Exit")
 
-    opt = sys.stdin.readline()
+    chosenOption = sys.stdin.readline()
     try:
-        opt = int(opt)
+        chosenOption = int(chosenOption)
     except ValueError as e:
         print("Please provide only numerical values.")
-        opt = 6
+        chosenOption = 6
 
-    if opt == 1:
-        renameCopyMoveFunc()
-    elif opt == 2:
-        renameCopyFunc()
-    elif opt == 3:
-        copyFunc()
-    elif opt == 4:
-        renameFunc()
-    elif opt == 5:
+    if chosenOption == 1:
+        renameCopyMove()
+    elif chosenOption == 2:
+        renameCopy()
+    elif chosenOption == 3:
+        copy()
+    elif chosenOption == 4:
+        rename()
+    elif chosenOption == 5:
         print("Exiting script...")
     else:
         print("Unknown value...")
-        opt = 6
+        chosenOption = 6
